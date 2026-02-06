c55cfb88ff46af9c73991ac389ecd2aed67e0f97
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-02-06 21:50:30 +0530

    update axios api config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
+
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
